cd8846d2954f5dbe147ea1d6860fd281f811f245
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-03-19 17:27:31 +0530

    Version 1.1

--- app/src/main/java/com/osos/eduwaive/MainActivity.java
+++ app/src/main/java/com/osos/eduwaive/MainActivity.java
@@ -3,12 +3,35 @@ package com.osos.eduwaive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+SignInButton googleButton;
+FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        googleButton=findViewById(R.id.GoogleSign);
+        auth=FirebaseAuth.getInstance();
+        GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("446602385687-depl5rvdih26nl9pte7qo53i98p7nk3j.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+
     }
 }
