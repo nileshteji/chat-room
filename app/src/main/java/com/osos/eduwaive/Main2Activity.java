6d4eeb63e3614d3836b658491f35214bbc689588
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-03-29 16:58:01 +0530

    Version 2.4

--- app/src/main/java/com/osos/eduwaive/Main2Activity.java
+++ app/src/main/java/com/osos/eduwaive/Main2Activity.java
@@ -18,6 +18,7 @@ import android.inputmethodservice.KeyboardView;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.FileUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -27,6 +28,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -49,6 +54,7 @@ FirebaseAuth auth;
 ProgressBar progressBar;
 DatabaseReference database;
 Button boj;
+GoogleApiClient mGoogleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +67,10 @@ Button boj;
         auth=FirebaseAuth.getInstance();
         boj=findViewById(R.id.button2);
         progressBar.setVisibility(View.VISIBLE);
+        mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext()) //Use app context to prevent leaks using activity
+                //.enableAutoManage(this /* FragmentActivity */, connectionFailedListener)
+                .addApi(Auth.GOOGLE_SIGN_IN_API)
+                .build();
 
         database.addValueEventListener(new ValueEventListener() {
             @Override
@@ -98,15 +108,25 @@ Button boj;
 
 
         boj.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                auth.signOut();
-                Intent boj=new Intent(Main2Activity.this,MainActivity.class);
-                boj.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                   @Override
+                                   public void onClick(View v) {
 
-                startActivity(boj);
-            }
-        });
+                                       if (mGoogleApiClient.isConnected()) {
+                                           Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+                                           mGoogleApiClient.disconnect();
+                                           mGoogleApiClient.connect();
+                                           Intent obj=new Intent(Main2Activity.this,MainActivity.class);
+                                           obj.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                           startActivity(obj);
+                                       }
+
+
+                                   }
+                               });
+
+
+
+
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -133,6 +153,49 @@ Button boj;
 
 
 
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+
+    public void clearApplicationData() {
+        File cache = getCacheDir();
+        File appDir = new File(cache.getParent());
+        if(appDir.exists()){
+            String[] children = appDir.list();
+            for(String s : children){
+                if(!s.equals("lib")){
+                    deleteDir(new File(appDir, s));
+                    Log.i("TAG", "File /data/data/APP_PACKAGE/" + s +" DELETED");
+                }
+            }
+        }
+    }
+
+    public static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+
+        return dir.delete();
     }
 
 
